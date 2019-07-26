4d0dca007ce9769c0e1ddd354a1789ae5808bfa6
david <davidk@users.noreply.github.com>
2019-07-25 21:20:32 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ echo '**************************************************************************
 echo
 sudo apt install git cmake libboost-dev libboost-regex-dev libboost-filesystem-dev libboost-thread-dev libboost-log-dev libboost-locale-dev libcurl4-openssl-dev libwxgtk3.0-dev build-essential pkg-config libtbb-dev zlib1g-dev
 echo 
-git clone https://github.com/AppImage/pkg2appimage && cp ps.yml pkg2appimage && cd pkg2appimage && ./pkg2appimage ps.yml
+[[ -d "./pkg2appimage" ]] || git clone https://github.com/AppImage/pkg2appimage && cp ps.yml ./pkg2appimage && cd pkg2appimage && SYSTEM_ARCH="armhf" ./pkg2appimage ps.yml
