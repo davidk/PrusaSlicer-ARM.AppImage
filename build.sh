c092525cb6c549edcdd42af70d3ac3719efda133
davidk <kdavid@gmail.com>
2019-08-12 16:24:12 -0700

    build.sh: Add libcereal-dev as a dependency

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo '**************************************************************************
 echo '* This utility needs permission to install the following things as dependencies *'
 echo '*********************************************************************************'
 echo
-if sudo apt install git cmake libboost-dev libboost-regex-dev libboost-filesystem-dev libboost-thread-dev libboost-log-dev libboost-locale-dev libcurl4-openssl-dev libwxgtk3.0-dev build-essential pkg-config libtbb-dev zlib1g-dev; then
+if sudo apt install git cmake libboost-dev libboost-regex-dev libboost-filesystem-dev libboost-thread-dev libboost-log-dev libboost-locale-dev libcurl4-openssl-dev libwxgtk3.0-dev build-essential pkg-config libtbb-dev zlib1g-dev libcereal-dev; then
   echo 
   [[ -d "./pkg2appimage" ]] || git clone https://github.com/AppImage/pkg2appimage && cp ps.yml ./pkg2appimage && cd pkg2appimage && SYSTEM_ARCH="armhf" ./pkg2appimage ps.yml
 fi
