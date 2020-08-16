b32e0c7de9a21e40c8d3c40b412ca5c9f85761fe
davidk <kdavid@gmail.com>
2020-08-16 03:48:18 -0700

    upload-release.sh: Shellcheck

--- upload-release.sh
+++ upload-release.sh
@@ -6,16 +6,18 @@
 # token="[github personal access token: https://github.com/settings/tokens]"
 # release_id="[id of a release to upload to, found here: https://api.github.com/repos/yours/PrusaSlicer-ARM.AppImage/releases]"
 # repo="yours/PrusaSlicer-ARM.AppImage"
+
+# shellcheck source=/dev/null
 source ~/.config/.githubconfig
 
 FILE="$1"
 
 [[ ! -e "$FILE" ]] && echo "usage: $0 [file to upload]" && exit 1;
 
-RELEASE_URL="https://uploads.github.com/repos/${repo}/releases/${release_id}/assets?name=$(basename $FILE)"
+RELEASE_URL="https://uploads.github.com/repos/${repo}/releases/${release_id}/assets?name=$(basename "${FILE}")"
 
 echo "Uploading ${FILE} to Github release ${release_id}"
 
-cat ${FILE} | curl -H "Authorization: token ${token}" \
-     -H "Content-Type: $(file -b --mime-type $FILE)" \
-     --data-binary @- ${RELEASE_URL}
+< "${FILE}" curl -H "Authorization: token ${token}" \
+     -H "Content-Type: $(file -b --mime-type "${FILE}")" \
+     --data-binary @- "${RELEASE_URL}"
