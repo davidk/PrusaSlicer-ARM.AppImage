b62bb7d1be9147e0c0222f792de9d673d8c76fb5
davidk <kdavid@gmail.com>
2021-05-22 10:23:23 -0700

    Fully integrate Dockerfile with container-build

--- container-build.sh
+++ container-build.sh
@@ -4,7 +4,7 @@
 # Git repository address where PrusaSlicer is located
 PS_GIT_REPO="https://github.com/prusa3d/PrusaSlicer"
 
-# The path where PrusaSlicer is located. If not set by a path on the CLI, the 
+# The path where PrusaSlicer is located. If not set by a path on the CLI, the
 # utility will clone a copy using Git and build it.
 BUILD_PS_IN=""
 
@@ -20,7 +20,7 @@ if [[ -z "$BUILD_PS_IN" ]]; then
 	fi
 
 	if [[ ! -d "./PrusaSlicer" ]]; then
-		git clone $PS_GIT_REPO 
+		git clone $PS_GIT_REPO
 	fi
 
 	BUILD_PS_IN=$(readlink -f ./PrusaSlicer)
@@ -40,25 +40,73 @@ if hash podman 2>/dev/null; then
 	${CONTAINER_BIN} unshare chown 1000:1000 -R "${BUILD_PS_IN}"
 fi
 
+echo "Init build container .."
+${CONTAINER_BIN} build -t keyglitch/prusaslicer-compiler - <<EOF
+FROM debian:buster
 
-if ! ${CONTAINER_BIN} build -t keyglitch/prusaslicer-compiler -f Dockerfile.development .; then
-	echo "Failed to build container; $?"
-	exit;
-fi
+ENV TZ=UTC
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+RUN apt-get update && apt-get install -y \
+  apt-transport-https \
+  build-essential \
+  cmake \
+  ca-certificates \
+  git \
+  libboost-dev \
+  libboost-regex-dev \
+  libboost-filesystem-dev \
+  libboost-thread-dev \
+  libboost-log-dev \
+  libboost-locale-dev \
+  libboost-iostreams-dev \
+  libgtk2.0-dev \
+  libgtk-3-dev \
+  libwxgtk3.0-gtk3-dev \
+  locales \
+  ninja-build \
+  m4 \
+  zlib1g-dev \
+  && rm -rf /var/lib/apt/lists/* \
+  && apt-get autoremove -y \
+  && apt-get autoclean
+
+RUN sed -i \
+  -e 's/^# \(cs_CZ\.UTF-8.*\)/\1/' \
+  -e 's/^# \(de_DE\.UTF-8.*\)/\1/' \
+  -e 's/^# \(en_US\.UTF-8.*\)/\1/' \
+  -e 's/^# \(es_ES\.UTF-8.*\)/\1/' \
+  -e 's/^# \(fr_FR\.UTF-8.*\)/\1/' \
+  -e 's/^# \(it_IT\.UTF-8.*\)/\1/' \
+  -e 's/^# \(ko_KR\.UTF-8.*\)/\1/' \
+  -e 's/^# \(pl_PL\.UTF-8.*\)/\1/' \
+  -e 's/^# \(uk_UA\.UTF-8.*\)/\1/' \
+  -e 's/^# \(zh_CN\.UTF-8.*\)/\1/' \
+  /etc/locale.gen \
+  && locale-gen
+
+RUN groupadd slic3r \
+  && useradd -g slic3r --create-home --home-dir /home/slic3r slic3r
+
+USER slic3r
+ENV USER slic3r
+WORKDIR /home/slic3r/
+EOF
 
 # Note, this relinks resources to be relative pathed instead of absolute since the inter-container path probably
 # differs from the path outside (if the container is run that way)
 ${CONTAINER_BIN} run -v "${BUILD_PS_IN}":/home/slic3r/PrusaSlicer:Z -i --rm keyglitch/prusaslicer-compiler <<EOF
-    cd /home/slic3r/PrusaSlicer \
-    && cd deps && mkdir -p deps-build && cd deps-build \
-    && cmake -G Ninja .. \
-    && ninja \
-    && cd ../.. && mkdir -p build && cd build \
-    && cmake .. -G Ninja -DCMAKE_PREFIX_PATH="${PWD}../deps/build/destdir/usr/local" -DSLIC3R_STATIC=1 \
-    && ninja \
-    && rm -f resources \
-    && ln -sr ../resources resources \
-    && readlink -f ./src/prusa-slicer \
-    && exit 0
+  cd /home/slic3r/PrusaSlicer \
+  && cd deps && mkdir -p build && cd build \
+  && cmake -G Ninja .. \
+  && ninja \
+  && cd ../.. && mkdir -p build && cd build \
+  && cmake .. -G Ninja -DCMAKE_PREFIX_PATH="/home/slic3r/PrusaSlicer/deps/build/destdir/usr/local" -DSLIC3R_STATIC=1 \
+  && ninja \
+  && rm -f resources \
+  && ln -sr ../resources resources \
+  && exit 0
 EOF
 
+echo "Build finished. prusa-slicer can be found in:"
+readlink -f PrusaSlicer/build/src/prusa-slicer
