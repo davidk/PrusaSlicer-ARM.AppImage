ea2a8d81938215c704e0dac2f3202786b905d6ea
davidk <kdavid@gmail.com>
2021-09-13 01:13:04 -0700

    Add modifications for ARM to builder

--- ps-appimagetool-build.sh
+++ ps-appimagetool-build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-git clone https://github.com/prusa3d/PrusaSlicer --single-branch --branch version_2.4.0-alpha1 --depth 1 PrusaSlicer 
+# TODO(davidk): Add ways to rewrite certain options before building:
+# - version
+# - gtk version
+# - arch to build/package for
 
-ORIG_CWD=$(pwd)
+git clone https://github.com/prusa3d/PrusaSlicer --single-branch --branch version_2.4.0-alpha1 --depth 1 PrusaSlicer 
 
 cd PrusaSlicer/deps && \
 	mkdir -p build && \
@@ -33,19 +36,16 @@ AppDir:
     name: PrusaSlicer
     # TODO(davidk): Replace with PrusaSlicer's icon
     icon: utilities-terminal
-    version: "1.0"
+    version: version_2.4.0-alpha1
     exec: usr/bin/prusa-slicer
 
   apt:
-    arch: amd64 
+    arch: arm64
     allow_unauthenticated: true
     sources:
-#      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian buster main contrib non-free'
-#      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian-security/ buster/updates main contrib non-free'
-#      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian buster-updates main contrib non-free'
-      - sourceline: 'deb http://deb.debian.org/debian buster main contrib non-free'
-      - sourceline: 'deb http://deb.debian.org/debian-security/ buster/updates main contrib non-free'
-      - sourceline: 'deb http://deb.debian.org/debian buster-updates main contrib non-free'
+      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian buster main contrib non-free'
+      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian-security/ buster/updates main contrib non-free'
+      - sourceline: 'deb [arch=arm64] http://deb.debian.org/debian buster-updates main contrib non-free'
 
     include:
       - python
@@ -81,33 +81,8 @@ AppDir:
 AppImage: 
   update-information: None
   sign-key: None
-  arch: x86_64
-  #  arch: arm_aarch64
+  arch: aarch64
 
-#ingredients:
-#  dist: buster
-#  script:
-#    - [ -d "PrusaSlicer-build" ] || git clone https://github.com/prusa3d/PrusaSlicer --single-branch --branch VERSION_PLACEHOLDER --depth 1 PrusaSlicer-build
-#    - cd PrusaSlicer-build && mkdir -p rpi-build && cd rpi-build && cmake .. -DSLIC3R_WX_STABLE=1 -DSLIC3R_FHS=1 -DSLIC3R_GTK=PLACEHOLDER_GTK_VERSION && time make -j$(nproc)
-#script:
-#  # Workaround when using PrusaSlicer on X-over-SSH (and compiled with GTK3). Basic theme and icon sets.
-#  - mkdir -p ./usr/share/mime/packages && cp -a /usr/share/mime/packages/freedesktop.org.xml ./usr/share/mime/packages/
-#  - update-mime-database ./usr/share/mime
-#  - mkdir -p ./usr/share/icons/Adwaita/ && cp -a /usr/share/icons/Adwaita/* ./usr/share/icons/Adwaita/
-#  - mkdir -p ./usr/share/glib-2.0/schemas && cp -a /usr/share/glib-2.0/schemas/* ./usr/share/glib-2.0/schemas
-#
-#  - mkdir -p ./usr/local/share/PrusaSlicer && cp -a ../PrusaSlicer-build/resources/* ./usr/local/share/PrusaSlicer/
-#  - mkdir -p ./usr/bin/ && cp -R ../PrusaSlicer-build/rpi-build/src/prusa-slicer ./usr/bin/
-#  - cp ../PrusaSlicer-build/resources/icons/PrusaSlicer.png PrusaSlicer.png
-#  - cat > ./PrusaSlicer.desktop <<EOF
-#  - [Desktop Entry]
-#  - Name=PrusaSlicer
-#  - Exec=prusa-slicer
-#  - Icon=PrusaSlicer
-#  - Terminal=false
-#  - Type=Application
-#  - Categories=Graphics;3DGraphics;
-#  - EOF
 EOF
 
 appimage-builder
