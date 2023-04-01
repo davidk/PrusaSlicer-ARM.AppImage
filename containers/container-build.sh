901cffef9377866541f224c6478ccd75937c736f
davidk <kdavid@gmail.com>
2023-04-01 19:45:18 +0000

    container-build: Add a 'seq'uential build mode for aarch64
    Specifying ./container-build.sh seq will trigger a sequential build
    for systems that do not have enough memory headroom to run both the
    aarch64 and armhf builders at the same time

--- containers/container-build.sh
+++ containers/container-build.sh
@@ -9,16 +9,17 @@
 # system impossible.
 #
 # Usage: ./container-build.sh 
+# Sequential builds: ./container-build.sh seq
 #
 # Test system: Radxa Rock 5B + native NVMe storage, optioned w/16GB RAM
 #
 DPKG_ARCH="$(dpkg --print-architecture)"
 
 if hash podman; then
-  echo "Detected Podman container runtime."
+  echo "Detected Podman container runtime under ${DPKG_ARCH} .."
   RUNTIME="podman"
 elif hash docker; then
-  echo "Detected lDocker container runtime."
+  echo "Detected Docker container runtime under ${DPKG_ARCH} .."
   RUNTIME="docker"
 else 
   echo "Please install podman or docker container tooling on this system to proceed."
@@ -28,9 +29,12 @@ fi
 cp ../build.sh ./
 
 if [[ "${DPKG_ARCH}" == "armhf" ]]; then
+  echo "Generating builder images for armhf .."
   ${RUNTIME} build -t psbuilder-armhf -f Dockerfile.armhf .
 elif [[ "${DPKG_ARCH}" == "arm64" ]]; then
+  echo "Generating builder images for armhf and aarch64.."
   ${RUNTIME} build -t psbuilder-armhf -f Dockerfile.armhf .
+
   ${RUNTIME} build -t psbuilder-aarch64 -f Dockerfile.aarch64 .
 else
   echo "Unknown architecture [arch: ${DPKG_ARCH}]."
@@ -38,41 +42,51 @@ else
 fi
 
 rm -f ./build.sh
-
 cd ../
 
 if [[ "${DPKG_ARCH}" == "armhf" ]]; then
-  { time ${RUNTIME} run --device /dev/fuse --cap-add SYS_ADMIN -it -v "${PWD}:/ps:z" psbuilder-armhf; } |& tee -a containers/armhf-build.log
+
+  { time ${RUNTIME} run --device /dev/fuse --cap-add SYS_ADMIN -it -v "${PWD}:/ps:z" psbuilder-armhf |& tee -a containers/armhf-build.log; }
   mv PrusaSlicer PrusaSlicer-armhf
+
 elif [[ "${DPKG_ARCH}" == "arm64" ]]; then
 
   CURNAME=$(basename "$(pwd)")
+
   echo "Base directory is ${CURNAME}. Creating ${CURNAME}-armhf and ${CURNAME}-aarch64 for concurrent builds .."
   cd ../
 
-  echo "Creating/copying ${CURNAME}-armhf / aarch64 .."
+  echo "Creating/copying ${CURNAME}-armhf / ${CURNAME}-aarch64 .."
+
   rsync -aW "${CURNAME}/" "${CURNAME}-armhf/" &
   rsync -aW "${CURNAME}/" "${CURNAME}-aarch64/" &
 
   wait
 
   # aarch64 
-
   cd "${CURNAME}-aarch64" || exit
   if [[ -d "PrusaSlicer-aarch64" ]]; then
     mv PrusaSlicer-aarch64 PrusaSlicer
   fi
 
-  { time ${RUNTIME} run -it -v "${PWD}:/ps:z" psbuilder-aarch64 |& tee -a containers/aarch64-build.log && mv PrusaSlicer PrusaSlicer-aarch64; }& 
+  if [[ $1 == "seq" ]]; then
+    { time ${RUNTIME} run -it -v "${PWD}:/ps:z" psbuilder-aarch64 |& tee -a containers/aarch64-build.log && mv PrusaSlicer PrusaSlicer-aarch64; }
+  else
+    { time ${RUNTIME} run -it -v "${PWD}:/ps:z" psbuilder-aarch64 |& tee -a containers/aarch64-build.log && mv PrusaSlicer PrusaSlicer-aarch64; }&
+  fi
 
   # armhf
-
   cd "../${CURNAME}-armhf" || exit
+
   if [[ -d "PrusaSlicer-armhf" ]]; then
     mv PrusaSlicer-armhf PrusaSlicer
   fi
 
-  { time setarch -B linux32 ${RUNTIME} run --device /dev/fuse --cap-add SYS_ADMIN -it -v "${PWD}:/ps:z" psbuilder-armhf |& tee -a containers/armhf-build.log && mv PrusaSlicer PrusaSlicer-armhf; }& 
+  if [[ $1 == "seq" ]]; then
+    { time setarch -B linux32 ${RUNTIME} run --device /dev/fuse --cap-add SYS_ADMIN -it -v "${PWD}:/ps:z" psbuilder-armhf |& tee -a containers/armhf-build.log && mv PrusaSlicer PrusaSlicer-armhf; } 
+  else
+    { time setarch -B linux32 ${RUNTIME} run --device /dev/fuse --cap-add SYS_ADMIN -it -v "${PWD}:/ps:z" psbuilder-armhf |& tee -a containers/armhf-build.log && mv PrusaSlicer PrusaSlicer-armhf; }&
+  fi
 
   cd ../
 
