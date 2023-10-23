cd231c1fcd776a1575b3b87ed5228f46f07af0bf
davidk <kdavid@gmail.com>
2023-10-22 22:52:55 -0700

    clean.sh: Detect container runtime before cleanup

--- clean.sh
+++ clean.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
+if hash podman; then
+  RUNTIME="podman"
+elif hash docker; then
+  RUNTIME="docker"
+else
+  echo "Container runtime not detected. Exiting (this is intended to clean up artifacts produced by containers/container-build.sh)."
+  exit 1;
+fi
+
 echo "Stopping and removing any running builds within podman containers .."
-podman kill psarm32 
-podman kill psarm64
-podman rm psarm32 
-podman rm psarm64
+${RUNTIME} kill psarm32 
+${RUNTIME} kill psarm64
+${RUNTIME} rm psarm32 
+${RUNTIME} rm psarm64
 echo "Removing PrusaSlicerBuild-* directories .."
 rm -rf PrusaSlicerBuild-*
 echo "Removing container images .."
-podman rmi localhost/psbuilder-armhf 
-podman rmi localhost/psbuilder-aarch64
+${RUNTIME} rmi localhost/psbuilder-armhf 
+${RUNTIME} rmi localhost/psbuilder-aarch64
 echo "Removing log files .."
 rm -f aarch64-build.log armhf-build.log
 echo "Done, you may now start a new build .."
