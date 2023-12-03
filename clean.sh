cd197c97f0e53284e593ee20ceb66e7e43ffd58e
davidk <kdavid@gmail.com>
2023-12-03 01:18:17 -0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if hash podman; then
   RUNTIME="podman"
@@ -12,13 +12,16 @@ fi
 echo "Stopping and removing any running builds within podman containers .."
 ${RUNTIME} kill psarm32 
 ${RUNTIME} kill psarm64
+${RUNTIME} kill psamd64
 ${RUNTIME} rm psarm32 
 ${RUNTIME} rm psarm64
+${RUNTIME} rm psamd64
 echo "Removing PrusaSlicerBuild-* directories .."
 rm -rf PrusaSlicerBuild-*
 echo "Removing container images .."
 ${RUNTIME} rmi localhost/psbuilder-armhf 
 ${RUNTIME} rmi localhost/psbuilder-aarch64
+${RUNTIME} rmi localhost/psbuilder-amd64
 echo "Removing log files .."
-rm -f aarch64-build.log armhf-build.log
+rm -f aarch64-build.log armhf-build.log amd64-build.log
 echo "Done, you may now start a new build .."
